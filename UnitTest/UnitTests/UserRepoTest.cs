dafe30a643fbe727bfc7873cde9ec6ca7f36f474
davidbeales <david.beales@ukho.gov.uk>
2017-12-12 13:57:29 +0000

    Refactored user repo unit tests.

--- UnitTest/UnitTests/UserRepoTest.cs
+++ UnitTest/UnitTests/UserRepoTest.cs
@@ -16,42 +16,40 @@ namespace UnitTest.UnitTests
     [TestFixture]
     public class UserRepoTest
     {
+        private User _user;
+        private string _path;
+        private List<User> _users = new List<User>();
+
+        [SetUp]
+        public void SettingUp()
+        {
+            _user = new User() { UserGuid = Guid.NewGuid(), Username = "davebeales", AccountGuid = new List<Guid>() };
+            _path = "somePath.txt";
+            _users.Add(_user);
+        }
+
         [Test]
         public void Can_Get_An_Exisiting_Valid_User()
         {
-            var path = "somePath.txt";
-            var username = "dave";
-            IEnumerable<User> users = new List<User>()
-            {
-                new User() { UserGuid = Guid.NewGuid(), Username = username }
-            };
             var fakeReaderWriter = A.Fake<IReaderWriter>();
+            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(_path)).Returns(_users);
 
-            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(path)).Returns(users);
+            var userRepo = new UserRepo(fakeReaderWriter, _path);
+            var result = userRepo.GetUser(_user.Username);
 
-            var userRepo = new UserRepo(fakeReaderWriter, path);
-            var result = userRepo.GetUser(username);
-
-            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(path)).MustHaveHappened(Repeated.Exactly.Once);
+            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(_path)).MustHaveHappened(Repeated.Exactly.Once);
         }
 
         [Test]
         public void Can_Add_A_Valid_User()
         {
-            var path = "somePath.txt";
-            var username = "davebeales";
-            IEnumerable<User> users = new List<User>()
-            {
-                new User() { UserGuid = Guid.NewGuid(), Username = username }
-            };
             var fakeReaderWriter = A.Fake<IReaderWriter>();
+            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(_path)).Returns(null);
 
-            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(path)).Returns(null);
+            var userRepo = new UserRepo(fakeReaderWriter, _path);
+            var isUserAdded = userRepo.AddUser(_user.Username);
 
-            var userRepo = new UserRepo(fakeReaderWriter, path);
-            var isUserAdded = userRepo.AddUser(username);
-
-            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(path)).MustHaveHappened(Repeated.Exactly.Times(1));
+            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(_path)).MustHaveHappened(Repeated.Exactly.Times(1));
             A.CallTo(() => fakeReaderWriter.WriteEnumerable(A<string>.Ignored, A< IEnumerable<User>>.Ignored)).MustHaveHappened(Repeated.Exactly.Times(1));
             Assert.That(true, Is.EqualTo(isUserAdded));
         }
@@ -59,35 +57,25 @@ namespace UnitTest.UnitTests
         [Test]
         public void Adding_An_Account_To_An_Exisiting_User_Returns_One_Account_For_User()
         {
-            var path = "somePath.txt";
-            var username = "dave";
             var accountGuid = Guid.NewGuid();
-            IEnumerable<User> users = new List<User>()
-            {
-               new User() {UserGuid = Guid.NewGuid(), Username = username, AccountGuid = new List<Guid>() }
-            };
             var fakeReaderWriter = A.Fake<IReaderWriter>();
-            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(path)).Returns(users);
+            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(_path)).Returns(_users);
             
-            var userRepo = new UserRepo(fakeReaderWriter, path);
-            userRepo.AddAccount(username, accountGuid);
+            var userRepo = new UserRepo(fakeReaderWriter, _path);
+            userRepo.AddAccount(_user.Username, accountGuid);
 
             A.CallTo(() => fakeReaderWriter.WriteEnumerable(A<string>.Ignored, A<IEnumerable<User>>.Ignored)).MustHaveHappened(Repeated.Exactly.Times(1));
-            Assert.That(1, Is.EqualTo(userRepo.GetUser(username).AccountGuid.Count));
+            Assert.That(1, Is.EqualTo(userRepo.GetUser(_user.Username).AccountGuid.Count));
         }
 
         [Test]
         public void Remove_An_Exisitng_User_On_Delete_User()
         {
-            var path = "somePath.txt";
-            var username = "dave";
-            var user = new User() {UserGuid = Guid.NewGuid(), Username = username, AccountGuid = new List<Guid>()};
-            IEnumerable<User> users = new List<User>() {user};
             var fakeReaderWriter = A.Fake<IReaderWriter>();
-            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(path)).Returns(users);
+            A.CallTo(() => fakeReaderWriter.ReadEnumerable<User>(_path)).Returns(_users);
 
-            var userRepo = new UserRepo(fakeReaderWriter, path);
-            var isUserDeleted = userRepo.DeleteUser(username);
+            var userRepo = new UserRepo(fakeReaderWriter, _path);
+            var isUserDeleted = userRepo.DeleteUser(_user.Username);
 
             Assert.That(true, Is.EqualTo(isUserDeleted));
         }
