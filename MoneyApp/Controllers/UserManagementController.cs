41982c6d61939d2f6ec6b938bd85ab83cb280a17
davidbeales <david.beales@ukho.gov.uk>
2017-12-14 15:23:17 +0000

    pre bower

---
+++ MoneyApp/Controllers/UserManagementController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyApp.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public UserManagementController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index", null);
+        }
+    }
+}
